2e3d5b0501b6edf914669336cc22ea437f3e0913
GowthamA1963 <aa9366@srmist.edu.in>
2025-11-01 13:16:26 +0530

    Added andino_gz simulation folder

--- andino_bringup/launch/teleop_keyboard.launch.py
+++ andino_bringup/launch/teleop_keyboard.launch.py
@@ -38,6 +38,7 @@ def generate_launch_description():
             name='teleop_twist_keyboard_node',
             output='screen',
             prefix = 'xterm -e',
+            use_sim_time=True
          )
 
     return LaunchDescription([
